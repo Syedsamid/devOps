97712b3c101304876598ff9f949e089bea2b27a6
Syedsamid <samidsyed1720@gmail.com>
2025-02-07 11:02:47 +0000

    how to deploy a project on website in React

---
+++ mern/client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+   <>
+     <h1>This is samid Website</h1>
+     <br />
+
+    <button class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 animate-bounce absolute op-5 left-10 ">Purple to Blue</button>
+
+    <p>Hello samid</p>
+   </>
+  )
+}
+
+export default App
